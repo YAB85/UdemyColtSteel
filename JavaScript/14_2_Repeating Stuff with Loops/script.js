f8abbf01c70ccc31d740db4a96c9d40642d10d72
yaroslavBova <yabova@ukr.net>
2021-02-18 23:58:13 +0200

    continue section about loops

--- JavaScript/14_2_Repeating Stuff with Loops/script.js
+++ JavaScript/14_2_Repeating Stuff with Loops/script.js
@@ -11,7 +11,7 @@ for (let i = 0; i < 6; i++) {
   console.log("Da ba deeda ba daa");
 } */
 
-for (let i = 0; i < 100; i++) {
+/* for (let i = 0; i < 100; i++) {
   if (i % 2 === 0) {
     console.log("Foo");
   } else if (i % 4 === 0) {
@@ -19,4 +19,42 @@ for (let i = 0; i < 100; i++) {
   } else if (i % 3 === 0 && i % 5 === 0) {
     console.log("Foo bar");
   }
+} */
+
+/* let arr = [];
+
+for (let i = 25; i >= 0; i -= 5) {
+  //arr.push(i);
+  console.log(i);
+}
+
+console.log(arr); */
+
+//const people = ["Scooby", "Velma", "Daphne", "Shaggy", "Fred"];
+
+/* for (let i = 0; i < people.length; i++) {
+  console.log(people[i].toUpperCase());
+} */
+
+/* for (let i = 1; i < 3; i++) {
+  console.log(`First level --- ${i}`);
+  for (let j = 1; j < 4; j++) {
+    console.log(`Second level -- ${j}`);
+    for (let k = 1; k < 5; k++) {
+      console.log(`Third level - ${k}`);
+    }
+  }
+} */
+let str = "";
+
+for (let i = 0; i < 8; i++) {
+  str += "#";
+  console.log(str);
+}
+
+let num = 0;
+
+while (num < 10) {
+  console.log(num);
+  num++;
 }
