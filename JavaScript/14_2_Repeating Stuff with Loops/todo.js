2a67b083c6d2eb80f9d5348bb812f90a5c5931dd
yaroslavBova <yabova@ukr.net>
2021-02-21 17:08:29 +0200

    finished section about arrays

---
+++ JavaScript/14_2_Repeating Stuff with Loops/todo.js
@@ -0,0 +1,30 @@
+console.log("Hi");
+
+let input = prompt("What would you like to do?");
+
+const todos = [];
+
+while (input !== "quit" && input !== "q") {
+  if (input === "list") {
+    console.log("*****************");
+    for (let i = 0; i < todos.length; i++) {
+      console.log(`${i}: ${todos[i]}`);
+    }
+    console.log("*****************");
+  } else if (input === "new") {
+    const newTodo = prompt("Ok, what is the new todo?");
+    todos.push(newTodo);
+    console.log(`${newTodo} added to the list`);
+  } else if (input === "delete" || input === "d") {
+    const index = parseInt(prompt("Ok, enter an index to delete"));
+    if (!Number.isNaN(index)) {
+      const deleted = todos.splice(index, 1);
+      console.log(`Ok, deleted ${deleted[0]}`);
+    } else {
+      console.log("Unknown index");
+    }
+  }
+  input = prompt("What would you like to do?");
+}
+
+console.log("Ok QUIT the APP!");
